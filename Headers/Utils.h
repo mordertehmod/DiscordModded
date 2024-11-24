8e6929bacd0492d8d3141d5f62f5e8acd01a54b1
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2024-11-24 10:57:10 +0100

    feat: improve and clean up recovery menu

--- Headers/Utils.h
+++ Headers/Utils.h
@@ -1,7 +1,22 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+@class BunnySettingsViewController;
+
 extern BOOL isJailbroken;
+BOOL isSafeModeEnabled(void);
 NSURL *getPyoncordDirectory(void);
 UIColor *hexToUIColor(NSString *hex);
+NSString *getDeviceIdentifier(void);
 void showErrorAlert(NSString *title, NSString *message, void (^completion)(void));
+void reloadApp(UIViewController *viewController);
+void deletePlugins(void);
+void deleteThemes(void);
+void deleteAllData(UIViewController *presenter);
+void refetchBundle(void);
+void toggleSafeMode(void);
+void setCustomBundleURL(NSURL *url, UIViewController *presenter);
+void resetCustomBundleURL(UIViewController *presenter);
+void showBundleSelector(UIViewController *presenter);
+void removeCachedBundle(void);
+void gracefulExit(UIViewController *presenter);
